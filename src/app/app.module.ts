49f8a6f76d09e03e0583c424290b584947a6abd0
kyra <kyradornish@gmail.com>
2019-08-11 22:21:06 -0400

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitialComponent } from './modules/initial/initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialComponent
   ],
   imports: [
     BrowserModule,
